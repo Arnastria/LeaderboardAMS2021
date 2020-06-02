4c7e9762ffe90ef6df1ea3be86803f7ecda0146c
Hadi Syah Putra <hadisyahputra30@live.com>
2020-06-02 16:29:03 +0800

    debug gender credential

--- web/upload_gender.php
+++ web/upload_gender.php
@@ -15,7 +15,7 @@ if ( isset($_POST["submit"]) and isset($_POST["uploadkey"])) {
 			$dbname = 'clgr9wl4akcxw07o';
 
 			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
+			$conn = new mysqli($host, $username, $password, $dbname);
 
 			// Check connection
 			if ($conn->connect_error) {
